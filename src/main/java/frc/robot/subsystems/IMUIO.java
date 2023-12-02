e14a8370226c855d2db8c4370c4efde071650fd0
Colin <cfinn68916@gmail.com>
2023-12-02 00:41:45 -0600

    Add IMU IO layer
    Signed-off-by: Colin <cfinn68916@gmail.com>

---
+++ src/main/java/frc/robot/subsystems/IMUIO.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IMUIO {
+  @AutoLog
+  public static class IMUIOInputs {
+    public double yaw = 0;
+    public double roll = 0;
+    public double pitch = 0;
+
+    public double xAccelMPS = 0.0;
+    public double yAccelMPS = 0.0;
+    public double zAccelMPS = 0.0;
+    public double tempC = 0.0;
+  }
+
+  public default void updateInputs(IMUIOInputs inputs) {};
+}
---
+++ src/main/java/frc/robot/subsystems/IMUIONavx.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+
+public class IMUIONavx implements IMUIO {
+  AHRS imu;
+
+  public IMUIONavx() {
+    imu = new AHRS(SPI.Port.kMXP);
+  }
+
+  @Override
+  public void updateInputs(IMUIOInputs inputs) {
+    inputs.tempC = imu.getTempC();
+    inputs.pitch = imu.getPitch();
+    inputs.yaw = imu.getYaw();
+    inputs.roll = imu.getRoll();
+    inputs.xAccelMPS = imu.getRawAccelX() * 9.8065;
+    inputs.yAccelMPS = imu.getRawAccelY() * 9.8065;
+    inputs.zAccelMPS = imu.getRawAccelZ() * 9.8065;
+  }
+}
