be3986b49bccc5c9a677b42d837cc70a1199ed7f
Colin <cfinn68916@gmail.com>
2023-12-19 17:56:14 -0600

    Deploy at 20231219_175614

--- src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
+++ src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
@@ -69,7 +69,7 @@ public class ModuleIOSparkFX implements ModuleIO {
     Logger.getInstance()
         .recordMetadata(name + "_Thrust_FW", String.valueOf(thrust.getFirmwareVersion()));
     Logger.getInstance().recordMetadata(name + "_Thrust_Name", thrust.getDescription());
-    steerOffset = Rotation2d.fromRotations(thrust.configGetCustomParam(0) / 1000000.0);
+    steerOffset = Rotation2d.fromRotations(thrust.configGetCustomParam(0) / 1000.0);
   }
 
   @Override
