0a872e7484a7e696fbb831653ee800055c6e2f64
Colin <cfinn68916@gmail.com>
2024-01-10 20:09:29 -0600

    Deploy at 20240110_200928

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -35,7 +35,7 @@ public class Vision extends PeriodicRunnable {
       for (int j = 0; j < outs[i].ids.length; j++) {
         if (outs[i].ids[j].length > 0) {
           Pose3d outPose = new Pose3d();
-          if (outs[i].ids[j].length != 1) {
+          if (outs[i].ids[j].length == 1) {
             Rotation3d r1 =
                 new Rotation3d(
                     new Quaternion(
@@ -50,7 +50,7 @@ public class Vision extends PeriodicRunnable {
                     drivetrain
                         .getPosition()
                         .getRotation()
-                        .interpolate(r1.toRotation2d(), 0)
+                        .interpolate(r1.toRotation2d(), 1)
                         .getRadians());
             Transform3d as =
                 new Transform3d(
@@ -63,7 +63,7 @@ public class Vision extends PeriodicRunnable {
                     .getTagPose(Integer.parseInt(outs[i].ids[j][0]))
                     .orElse(new Pose3d());
             outPose = tagpose.plus(as);
-          } else if (outs[i].ids[j].length >= 1) {
+          } else if (outs[i].ids[j].length > 1) {
             outPose =
                 new Pose3d(
                     outs[i].poses[j][0],
