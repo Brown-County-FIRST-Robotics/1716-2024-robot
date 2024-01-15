b2b993a2162e1b15e82bd805055a0728f21cf2d1
Colin <cfinn68916@gmail.com>
2024-01-14 21:58:56 -0600

    Add basic functions to Arm subsystem
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems.arm;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.LoggedTunableNumber;
 import org.littletonrobotics.junction.Logger;
 
 public class Arm extends SubsystemBase {
@@ -12,6 +14,8 @@ public class Arm extends SubsystemBase {
   MechanismLigament2d cmdArmStates = new MechanismLigament2d("Arm", 40, 0);
   ArmIO io;
   ArmIOInputsAutoLogged inputs = new ArmIOInputsAutoLogged();
+  Rotation2d cmdAng = new Rotation2d();
+  LoggedTunableNumber gravFF = new LoggedTunableNumber("Arm Gravity FF", 0.0);
 
   public Arm(ArmIO io) {
     realStates.getRoot("Root", 50, 50).append(realArmStates);
@@ -23,9 +27,33 @@ public class Arm extends SubsystemBase {
     Logger.processInputs("Arm/Inputs", inputs);
   }
 
+  public void setAngle(Rotation2d rot) {
+    cmdArmStates.setAngle(rot);
+    Logger.recordOutput("Arm/cmdState", cmdStates);
+    cmdAng = rot;
+    commandAngle();
+  }
+
+  private void commandAngle() {
+    io.setAngle(cmdAng, cmdAng.getCos() * gravFF.get());
+  }
+
+  public Rotation2d getAngle() {
+    return inputs.angle;
+  }
+
+  public void commandIncrement(Rotation2d inc) {
+    setAngle(getAngle().plus(inc));
+  }
+
   @Override
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("Arm/Inputs", inputs);
+    commandAngle();
+    cmdArmStates.setAngle(cmdAng);
+    realArmStates.setAngle(getAngle());
+    Logger.recordOutput("Arm/realState", realStates);
+    Logger.recordOutput("Arm/cmdState", cmdStates);
   }
 }
