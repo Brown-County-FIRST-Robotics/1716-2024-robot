52cc834e257c92b0935b8fe1ac617946c8c52ed7
Colin <cfinn68916@gmail.com>
2024-01-15 20:10:33 -0600

    Add some javadoc to ArmIO
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- src/main/java/frc/robot/subsystems/arm/ArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -5,16 +5,24 @@ import org.littletonrobotics.junction.AutoLog;
 
 public interface ArmIO {
   @AutoLog
-  public static class ArmIOInputs {
+  class ArmIOInputs {
     Rotation2d angle = new Rotation2d();
     double omega = 0.0;
     double appliedOutput = 0.0;
     double temperature = 0.0;
   }
+  /**
+   * Updates the inputs
+   *
+   * @param inputs A reference to the inputs to update
+   */
+  default void updateInputs(ArmIOInputs inputs) {}
 
-  public default void updateInputs(ArmIOInputs inputs) {}
-  ;
-
+  /**
+   * Commands an angle
+   *
+   * @param cmdAng The command angle
+   * @param arbFF The arbitrary feedforward for gravity compensation (in volts)
+   */
   default void setAngle(Rotation2d cmdAng, double arbFF) {}
-  ;
 }
