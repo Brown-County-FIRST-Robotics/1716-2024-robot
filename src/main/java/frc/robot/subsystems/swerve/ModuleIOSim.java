cc74320e06e85fae4dd81674297f4142bdd4bf68
Colin <cfinn68916@gmail.com>
2024-01-16 14:51:44 -0600

    Make ModuleIOSim convert to robot rotations for commanded state
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- src/main/java/frc/robot/subsystems/swerve/ModuleIOSim.java
+++ src/main/java/frc/robot/subsystems/swerve/ModuleIOSim.java
@@ -1,11 +1,19 @@
 package frc.robot.subsystems.swerve;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import frc.robot.SwerveSimManager;
 
 public class ModuleIOSim implements ModuleIO {
   private int id;
+  private static final Rotation2d[] chassisOffsets =
+      new Rotation2d[] {
+        Rotation2d.fromDegrees(-90),
+        Rotation2d.fromDegrees(0),
+        Rotation2d.fromDegrees(180),
+        Rotation2d.fromDegrees(90)
+      };
 
   public ModuleIOSim(int id) {
     this.id = id;
@@ -13,14 +21,22 @@ public class ModuleIOSim implements ModuleIO {
 
   @Override
   public void updateInputs(ModuleIOInputs inputs) {
-    inputs.pos = SwerveSimManager.getInstance().getModPos(id);
-    inputs.vel = SwerveSimManager.getInstance().getModState(id);
+
+    var realAng =
+        SwerveSimManager.getInstance().getModPos(id).angle.plus(chassisOffsets[id]).unaryMinus();
+    inputs.pos =
+        new SwerveModulePosition(
+            SwerveSimManager.getInstance().getModPos(id).distanceMeters, realAng);
+    inputs.vel =
+        new SwerveModuleState(
+            SwerveSimManager.getInstance().getModState(id).speedMetersPerSecond, realAng);
+    inputs.offset = 0;
   }
 
   @Override
   public void setCmdState(SwerveModuleState state) {
-    SwerveModulePosition pos = SwerveSimManager.getInstance().getModPos(id);
-    state.speedMetersPerSecond *= pos.angle.minus(state.angle).getCos();
-    SwerveSimManager.getInstance().commandState(id, state);
+    var realRot = state.angle.unaryMinus().minus(chassisOffsets[id]);
+    SwerveSimManager.getInstance()
+        .commandState(id, new SwerveModuleState(state.speedMetersPerSecond, realRot));
   }
 }
