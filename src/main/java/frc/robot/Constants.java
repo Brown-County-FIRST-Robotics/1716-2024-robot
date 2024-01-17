30a44e43b658b3d1284d23295d089606a8f0ecef
Colin <cfinn68916@gmail.com>
2024-01-16 23:00:03 -0600

    Add Javadoc to Constants
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,37 +17,54 @@ import edu.wpi.first.math.geometry.Rotation2d;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
+
+  /** Constants for autonomous operation */
+  public static final class Auto {
+    /** P value for the trajectory x PID loop */
+    public static final double X_P = 0;
+    /** I value for the trajectory x PID loop */
+    public static final double X_I = 0;
+    /** D value for the trajectory x PID loop */
+    public static final double X_D = 0;
+    /** P value for the trajectory y PID loop */
+    public static final double Y_P = 0;
+    /** I value for the trajectory y PID loop */
+    public static final double Y_I = 0;
+    /** D value for the trajectory y PID loop */
+    public static final double Y_D = 0;
+    /** P value for the trajectory theta PID loop */
+    public static final double THETA_P = 0;
+    /** I value for the trajectory theta PID loop */
+    public static final double THETA_I = 0;
+    /** D value for the trajectory theta PID loop */
+    public static final double THETA_D = 0;
+
+    /** The max angular velocity value in a trajectory feedback loop */
+    public static final double MAX_ANGULAR_VELOCITY = 1;
+    /** The max angular acceleration value in a trajectory feedback loop */
+    public static final double MAX_ANGULAR_ACCELERATION = 1;
+    /** The max velocity for trajectories */
+    public static final double MAX_VELOCITY = 5;
+    /** The max acceleration for trajectories */
+    public static final double MAX_ACCELERATION = 3;
+  }
+
+  /** Constants relating to manual operation */
+  public static final class Driver {
+    /** The maximum x velocity during manual operation */
+    public static final double MAX_X_SPEED = 4;
+    /** The maximum y velocity during manual operation */
+    public static final double MAX_Y_SPEED = 4;
+    /** The maximum angular velocity during manual operation */
+    public static final double MAX_THETA_SPEED = 6;
     /** The port the driver controller is on */
     public static final int DRIVER_CONTROLLER_PORT = 0;
   }
 
-  public static final class Auto {
-    public static final double X_P = 0;
-    public static final double X_I = 0;
-    public static final double X_D = 0;
-
-    public static final double Y_P = 0;
-    public static final double Y_I = 0;
-    public static final double Y_D = 0;
-
-    public static final double THETA_P = 0;
-    public static final double THETA_I = 0;
-    public static final double THETA_D = 0;
-
-    public static final double MAX_ANGULAR_VELOCITY = 1;
-    public static final double MAX_ANGULAR_ACCELERATION = 1;
-    public static final double MAX_VELOCITY = 5;
-    public static final double MAX_ACCELERATION = 3;
-  }
-
-  public static final class Driver {
-    public static final double MAX_X_SPEED = 4;
-    public static final double MAX_Y_SPEED = 4;
-    public static final double MAX_THETA_SPEED = 6;
-  }
-
+  /** The initial pose of the robot */
   public static final Pose2d INIT_POSE = new Pose2d(0, 0, Rotation2d.fromDegrees(180));
+  /** The initial pose of the robot when it is on the red alliance */
   public static final Pose2d RED_INIT_POSE = new Pose2d(0, 0, Rotation2d.fromDegrees(0));
+  /** The initial pose of the robot when it is on the blue alliance */
   public static final Pose2d BLUE_INIT_POSE = new Pose2d(0, 0, Rotation2d.fromDegrees(180));
 }
