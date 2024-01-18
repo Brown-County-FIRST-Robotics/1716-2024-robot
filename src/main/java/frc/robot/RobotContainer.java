4794b24c27812eaef1e699740aa3caa2433c7b73
Colin <cfinn68916@gmail.com>
2024-01-18 14:33:44 -0600

    Add basic shooter controller command
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ package frc.robot;
 import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
@@ -18,6 +19,8 @@ import frc.robot.subsystems.IMUIOPigeon;
 import frc.robot.subsystems.IMUIOSim;
 import frc.robot.subsystems.mecanum.MecanumDrivetrain;
 import frc.robot.subsystems.mecanum.MecanumIOSpark;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.shooter.ShooterIOSparkFlex;
 import frc.robot.subsystems.swerve.ModuleIOSim;
 import frc.robot.subsystems.swerve.ModuleIOSparkFX;
 import frc.robot.subsystems.swerve.SwerveDrivetrain;
@@ -71,6 +74,8 @@ public class RobotContainer {
         driveSys = new MecanumDrivetrain(new MecanumIOSpark(1, 2, 3, 4), new IMUIONavx());
     }
     useAlliance();
+    var sh = new Shooter(new ShooterIOSparkFlex(58, 0));
+    sh.setDefaultCommand(Commands.run(() -> sh.cmdVoltage(driverController.getLeftY() * 12.0), sh));
     driveSys.setDefaultCommand(new TeleopDrive(driveSys, driverController));
     configureBindings();
   }
