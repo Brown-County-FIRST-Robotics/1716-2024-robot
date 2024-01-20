bdbb19f65e35b78a3da2b52fb80c40adbad69599
Colin <cfinn68916@gmail.com>
2024-01-20 15:05:08 -0600

    Unstaged changes during deploy at 20240120_150508

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -3,14 +3,16 @@ package frc.robot.subsystems.shooter;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface ShooterIO {
+  void setVelocity(double vel);
+
   @AutoLog
   public static class ShooterIOInputs {
     boolean beamBroke = false;
-    double motorTemperature = 0.0;
-    double motorCurrent = 0.0;
-    double motorOutput = 0.0;
-    double position = 0.0;
-    double velocity = 0.0;
+    double[] motorTemperature = new double[]{0,0};
+    double[] motorCurrent = new double[]{0,0};
+    double[] motorOutput = new double[]{0,0};
+    double[] position = new double[]{0,0};
+    double[] velocity = new double[]{0,0};
   }
 
   default void updateInputs(ShooterIOInputs inputs) {}
