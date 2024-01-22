2342a3e1ecce03777c8c85a1f7e2f9477e608524
Colin <cfinn68916@gmail.com>
2024-01-22 16:36:29 -0600

    Unstaged changes during deploy at 20240122_163629

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -2,29 +2,45 @@ package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.LoggedDashboardBoolean;
+import org.littletonrobotics.junction.networktables.LoggedDashboardNumber;
 
 public class Shooter extends SubsystemBase {
-  ShooterIO io;
+  ShooterIO shooterIO;
+  FeederIO feederIO;
+  FeederIOInputsAutoLogged feederInputs = new FeederIOInputsAutoLogged();
 
-  ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
+  ShooterIOInputsAutoLogged shooterInputs = new ShooterIOInputsAutoLogged();
+  LoggedDashboardBoolean shouldReset = new LoggedDashboardBoolean("Reset arm", false);
+  LoggedDashboardNumber feederPos = new LoggedDashboardNumber("Arm Pos", 0);
 
-  public Shooter(ShooterIO io) {
-    this.io = io;
-    io.updateInputs(inputs);
-    Logger.processInputs("Shooter/Inputs", inputs);
+  public Shooter(ShooterIO io, FeederIO feederIO) {
+    this.shooterIO = io;
+    this.feederIO = feederIO;
+    io.updateInputs(shooterInputs);
+    Logger.processInputs("Shooter/ShooterInputs", shooterInputs);
+    feederIO.updateInputs(feederInputs);
+    Logger.processInputs("Shooter/FeederInputs", feederInputs);
   }
 
   @Override
   public void periodic() {
-    io.updateInputs(inputs);
-    Logger.processInputs("Shooter/Inputs", inputs);
+    shooterIO.updateInputs(shooterInputs);
+    Logger.processInputs("Shooter/ShooterInputs", shooterInputs);
+    feederIO.updateInputs(feederInputs);
+    Logger.processInputs("Shooter/FeederInputs", feederInputs);
+    if (shouldReset.get()) {
+      feederIO.resetPos();
+      shouldReset.set(false);
+    }
+    feederIO.cmdPos(feederPos.get());
   }
 
   public void cmdvel(double voltage) {
-    io.setVelocity(voltage);
+    shooterIO.setVelocity(voltage);
   }
 
   public void cmdVoltage(double voltage) {
-    io.setVoltage(voltage);
+    shooterIO.setVoltage(voltage);
   }
 }
