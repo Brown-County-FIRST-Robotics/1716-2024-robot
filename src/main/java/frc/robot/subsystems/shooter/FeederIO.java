f29d1dfc69bf9871d05949a8763c348f3c2f7deb
Colin <cfinn68916@gmail.com>
2024-01-22 16:42:54 -0600

    Unstaged changes during deploy at 20240122_164254

--- src/main/java/frc/robot/subsystems/shooter/FeederIO.java
+++ src/main/java/frc/robot/subsystems/shooter/FeederIO.java
@@ -1,4 +1,16 @@
 package frc.robot.subsystems.shooter;
 
+import org.littletonrobotics.junction.AutoLog;
+
 public interface FeederIO {
+  @AutoLog
+  public static class FeederIOInputs {
+    public double feederPos = 0.0;
+  }
+
+  default void updateInputs(FeederIOInputs inputs) {}
+
+  default void cmdPos(double pos) {}
+
+  default void resetPos() {}
 }
