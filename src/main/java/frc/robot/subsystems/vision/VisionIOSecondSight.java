64cdd17fff388857746b170bef464192a9fb3961
Colin <cfinn68916@gmail.com>
2024-01-31 23:04:31 -0600

    Unstaged changes during deploy at 20240131_230431

--- src/main/java/frc/robot/subsystems/vision/VisionIOSecondSight.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOSecondSight.java
@@ -3,6 +3,8 @@ package frc.robot.subsystems.vision;
 import edu.wpi.first.networktables.*;
 import edu.wpi.first.wpilibj.Timer;
 
+import java.util.Arrays;
+
 /** The IO layer for one SecondSight camera */
 public class VisionIOSecondSight implements VisionIO {
   BooleanSubscriber isRecordingSub;
@@ -40,6 +42,8 @@ public class VisionIOSecondSight implements VisionIO {
   public void updateInputs(VisionIOInputs inputs) {
     inputs.isRecording = isRecordingSub.get();
     inputs.recordingPath = recordingPathSub.get();
+    var unused=posesSub.readQueue();
+    System.out.println(Arrays.toString(unused));
     double[][] poses = posesSub.readQueueValues();
     String[][] ids = idsSub.readQueueValues();
     inputs.timestamps = new double[ids.length];
