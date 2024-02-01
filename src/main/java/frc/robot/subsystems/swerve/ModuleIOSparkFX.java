e87970392b9ba0cc8d58499840ee9490180399b9
Colin <cfinn68916@gmail.com>
2024-02-01 16:26:40 -0600

    Unstaged changes during deploy at 20240201_162640

--- src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
+++ src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.swerve;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityDutyCycle;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
@@ -19,6 +20,7 @@ public class ModuleIOSparkFX implements ModuleIO {
   private final SparkAnalogSensor encoder;
   private final SparkPIDController pid;
   private final TalonFX thrust;
+  double offset;
   String name;
   LoggedTunableNumber thrustP = new LoggedTunableNumber("Thrust P", 0);
   LoggedTunableNumber thrustI = new LoggedTunableNumber("Thrust I", 0);
@@ -41,14 +43,14 @@ public class ModuleIOSparkFX implements ModuleIO {
     thrust = new TalonFX(thrustID);
     TalonFXConfiguration config = new TalonFXConfiguration();
     config.Slot0.kV = thrustKV.get();
-    if (name == "FL") {
-      config.CustomParams.CustomParam0 = 825;
-    } else if (name == "FR") {
-      config.CustomParams.CustomParam0 = 5;
-    } else if (name == "BL") {
-      config.CustomParams.CustomParam0 = 982;
-    } else if (name == "BR") {
-      config.CustomParams.CustomParam0 = 456;
+    if (steerID == 10) {
+      offset = 825/1000.0;
+    } else if (steerID == 11) {
+      offset = 5/1000.0;
+    } else if (steerID == 12) {
+      offset = 982/1000.0;
+    } else if (steerID == 13) {
+      offset = 456/1000.0;
     }
     config.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
     config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
@@ -94,21 +96,22 @@ public class ModuleIOSparkFX implements ModuleIO {
     inputs.thrustErr = thrust.getClosedLoopError().getValue();
     inputs.thrustTempC = thrust.getDeviceTemp().getValue();
     //    inputs.offset = thrust.configGetCustomParam(0) / 1000.0;
-    if (name == "FL") {
-      inputs.offset = 0.825;
-    } else if (name == "FR") {
-      inputs.offset = 0.005;
-    } else if (name == "BL") {
-      inputs.offset = 0.982;
-    } else if (name == "BR") {
-      inputs.offset = 0.456;
-    }
+//    if (name == "FL") {
+//      inputs.offset = 0.825;
+//    } else if (name == "FR") {
+//      inputs.offset = 0.005;
+//    } else if (name == "BL") {
+//      inputs.offset = 0.982;
+//    } else if (name == "BR") {
+//      inputs.offset = 0.456;
+//    }
+    inputs.offset=offset;
   }
 
   @Override
   public void setCmdState(SwerveModuleState state) {
     double cmd_ang = state.angle.getRotations();
-    thrust.setControl(new VelocityVoltage(state.speedMetersPerSecond / THRUST_DISTANCE_PER_TICK));
+    thrust.setControl(new VelocityDutyCycle(state.speedMetersPerSecond / THRUST_DISTANCE_PER_TICK));
 
     pid.setReference(((cmd_ang % 1.0) + 1.0) % 1.0, CANSparkMax.ControlType.kSmartMotion);
   }
