c3eb93556b989fcf48ca92e31490499013411d0d
Colin <cfinn68916@gmail.com>
2024-02-07 19:37:52 -0600

    Fix shooting while moving, add sim manual test, and add a few utility functions
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- src/main/java/frc/robot/utils/ShootWhileMove.java
+++ src/main/java/frc/robot/utils/ShootWhileMove.java
@@ -3,6 +3,8 @@ package frc.robot.utils;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.geometry.Twist2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 public class ShootWhileMove {
   public interface ShooterKinematics {
@@ -15,6 +17,18 @@ public class ShootWhileMove {
     Translation2d getPoseAfterTime(double t);
 
     Translation2d getBotVelAfterTime(double t);
+  }
+
+  public static Translation2d getFieldRelativeSpeeds(ChassisSpeeds speeds, Rotation2d angle) {
+    return new Translation2d(speeds.vxMetersPerSecond, speeds.vyMetersPerSecond)
+        .rotateBy(angle.unaryMinus());
+  }
+
+  public static Twist2d discreteSpeeds(ChassisSpeeds speeds, double dt) {
+    return new Twist2d(
+        speeds.vxMetersPerSecond * dt,
+        speeds.vyMetersPerSecond * dt,
+        speeds.omegaRadiansPerSecond * dt);
   }
 
   public static class ShootingCommand {
@@ -42,9 +56,9 @@ public class ShootWhileMove {
   public static ShootingCommand calcSimpleCommand(
       Translation3d botPose, Translation2d botVelocity) {
     double g = 9.8065;
-    double tmpA = Math.sqrt(-g * botPose.getZ());
+    double tmpA = Math.sqrt(-g * botPose.getZ() * 2);
     Translation2d tmpB = botPose.toTranslation2d().times(-g / tmpA).minus(botVelocity);
-    double shooterVel = Math.sqrt(dot(tmpB, tmpB) - g * botPose.getZ());
+    double shooterVel = Math.sqrt(dot(tmpB, tmpB) - g * botPose.getZ() * 2);
     return new ShootingCommand(
         tmpB.getAngle(), Rotation2d.fromRadians(Math.asin(tmpA / shooterVel)), shooterVel);
   }
@@ -88,5 +102,41 @@ public class ShootWhileMove {
     }
     System.out.println("calcCommandWithPrediction did not converge");
     return lastCommand;
+  }
+
+  public static void main(String[] args) { // Basic testing script
+    Translation3d target = new Translation3d(0.458597, 5.544566, 2.1105114);
+    Translation3d botPose = new Translation3d(3, 5, 0);
+    Translation2d botvel = new Translation2d(0, 0);
+    var cmd = calcSimpleCommand(botPose.minus(target), botvel);
+    System.out.println(cmd.shooterSpeedMPS);
+    Translation3d pose = botPose;
+    Translation3d vel = new Translation3d(botvel.getX(), botvel.getY(), 0);
+    double tVel = cmd.shooterSpeedMPS * cmd.shooterAngle.getCos();
+    vel =
+        vel.plus(
+            new Translation3d(
+                tVel * cmd.botAngle.getCos(),
+                tVel * cmd.botAngle.getSin(),
+                cmd.shooterSpeedMPS * cmd.shooterAngle.getSin()));
+    double dt = 0.001;
+    for (int i = 0; i < 10 / dt; i++) {
+      pose = pose.plus(vel.times(dt));
+      vel = vel.plus(new Translation3d(0, 0, -9.8065 * dt));
+      if (pose.minus(target).getNorm() < 0.1) {
+        System.out.println(
+            "t:"
+                + i * dt
+                + "\tx:"
+                + pose.getX()
+                + "\ty:"
+                + pose.getY()
+                + "\tz:"
+                + pose.getZ()
+                + "\tvz:"
+                + vel.getZ());
+      }
+    }
+    System.out.println(9.88 > cmd.shooterSpeedMPS);
   }
 }
