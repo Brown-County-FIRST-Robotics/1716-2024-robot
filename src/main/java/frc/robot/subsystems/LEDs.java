967767e647c51f3d914b06638367f6708f74aff8
RedBirdRoboticsBot <114703348+Isma27322@users.noreply.github.com>
2024-02-12 17:02:45 -0600

    made lights do snake movement

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -8,27 +8,45 @@ public class LEDs extends PeriodicRunnable {
   AddressableLED leds;
   AddressableLEDBuffer ledBuff;
   int counter = 0;
+  int brightness = 25;
+  int x = 5;
 
   public LEDs() {
     super(); // Super call adds it to the registry, which calls the periodic method every tick
     leds = new AddressableLED(5);
-    ledBuff = new AddressableLEDBuffer(10);
+    ledBuff = new AddressableLEDBuffer(50); //something around 280 length for LED
     leds.setLength(ledBuff.getLength());
     leds.setData(ledBuff);
     leds.start();
+    resetLeds();
   }
 
   @Override
-  public void periodic() {
+  public void periodic() { 
+    brightness = 25;
+   x = x + 1 % ledBuff.getLength();
+    
+   /*  for (var i = 0; i < 50; i++) { //mode 1
+      ledBuff.setHSV(i, (counter + i * 2) % 180, 255, brightness);
 
-    SetColor(counter, 255, 5);
-    counter += 5;
-  }
-
-  private void SetColor(int hue, int saturation, int value) {
-    for (var i = 0; i < ledBuff.getLength(); i++) {
-      ledBuff.setHSV(i, (hue - (20 * i)) % 180, saturation, value);
     }
+    counter += 2; */
+
+    ledBuff.setHSV((x - 5), 7 % 180, 255, 0);
+  
+  ledBuff.setHSV(x, 7 % 180, 255, brightness);
+   
+
+    counter += 2;
+
     leds.setData(ledBuff);
   }
-}
+
+  private void resetLeds() {
+    for(int i = 0; i < ledBuff.getLength(); i++){
+brightness = 0;
+    }
+    }
+
+  }
+
