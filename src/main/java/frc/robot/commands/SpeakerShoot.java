0be9460e5d3fa00b72cf680dd6d4daa73cd8c746
Colin <cfinn68916@gmail.com>
2024-02-14 17:17:41 -0600

    Unstaged changes during deploy at 20240214_171741

--- src/main/java/frc/robot/commands/SpeakerShoot.java
+++ src/main/java/frc/robot/commands/SpeakerShoot.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.FieldConstants;
 import frc.robot.subsystems.Drivetrain;
@@ -19,9 +20,10 @@ public class SpeakerShoot extends Command {
   Consumer<Optional<Rotation2d>> rotationCommander;
   Shooter shooter;
   boolean firing = false;
-  LoggedTunableNumber shooterAngleThreshold = new LoggedTunableNumber("ang threshold", 0.05);
-  LoggedTunableNumber botAngleThreshold = new LoggedTunableNumber("bot ang threshold", 0.05);
+  LoggedTunableNumber shooterAngleThreshold = new LoggedTunableNumber("ang threshold", 0.01);
+  LoggedTunableNumber botAngleThreshold = new LoggedTunableNumber("bot ang threshold", 0.02);
   LoggedTunableNumber botAngleThresho2ld = new LoggedTunableNumber("shoot fac2222", 0.8);
+  Timer waiter=new Timer();
 
   public SpeakerShoot(
       Drivetrain drive,
@@ -43,22 +45,33 @@ public class SpeakerShoot extends Command {
   @Override
   public void execute() {
     Pose2d pos = drive.getPosition();
-    Translation3d botPose = new Translation3d(pos.getX(), pos.getY(), 0.5);
-    Logger.recordOutput("Speak",new Pose3d(FieldConstants.getSpeaker(),new Rotation3d()));
-    var cmd =
-        ShootWhileMove.calcSimpleCommand(
-            botPose.minus(FieldConstants.getSpeaker()),
-            ShootWhileMove.getFieldRelativeSpeeds(
-                drive.getVelocity(), drive.getPosition().getRotation()));
-    shooter.commandSpeed(cmd.shooterSpeedMPS);
-    Logger.recordOutput("Drive/CMDState",new Pose2d(pos.getTranslation(),cmd.botAngle));
-    rotationCommander.accept(Optional.of(cmd.botAngle));
-    arm.setAngle(cmd.shooterAngle.times(botAngleThresho2ld.get()));
-    shooter.setFiringBlocked(
-        botAngleThreshold.get()
-                < Math.abs(cmd.botAngle.minus(drive.getPosition().getRotation()).getRotations())
+    Translation3d botPose = new Pose3d(pos.getX(), pos.getY(), 0,new Rotation3d(0,0,pos.getRotation().getRadians()))
+            .transformBy(new Transform3d(new Translation3d(0.3,0,0.3),new Rotation3d(0,-arm.getAngle().getRadians(),0)))
+            .transformBy(new Transform3d(new Translation3d(0.3,0,0.18),new Rotation3d())).getTranslation();
+    Logger.recordOutput("PredPose",new Pose3d(botPose,new Rotation3d(0,-arm.getAngle().getRadians(),0)));
+    var botAngle=FieldConstants.getSpeaker().minus(botPose).toTranslation2d().getAngle();
+    var shooterAngle=new Rotation2d(FieldConstants.getSpeaker().minus(botPose).toTranslation2d().getNorm(),FieldConstants.getSpeaker().minus(botPose).getZ());
+    shooter.commandSpeed(9.88);
+    rotationCommander.accept(Optional.of(botAngle));
+    arm.setAngle(shooterAngle);
+//    Bad Shooting While Moving Code
+//    var cmd =
+//        ShootWhileMove.calcSimpleCommand(
+//            botPose.minus(FieldConstants.getSpeaker()),
+//            ShootWhileMove.getFieldRelativeSpeeds(
+//                drive.getVelocity(), drive.getPosition().getRotation()));
+//    shooter.commandSpeed(cmd.shooterSpeedMPS);
+//    Logger.recordOutput("Drive/CMDState",new Pose2d(pos.getTranslation(),cmd.botAngle));
+//    rotationCommander.accept(Optional.of(cmd.botAngle));
+//    arm.setAngle(cmd.shooterAngle.times(botAngleThresho2ld.get()));
+    boolean blocked=botAngleThreshold.get()
+                < Math.abs(botAngle.minus(drive.getPosition().getRotation()).getRotations())
             || shooterAngleThreshold.get()
-                < Math.abs(cmd.shooterAngle.minus(arm.getAngle()).getRotations()));
+                < Math.abs(shooterAngle.minus(arm.getAngle()).getRotations())
+    || shooterAngleThreshold.get()<Math.abs(arm.getOmega());
+    if(!blocked){waiter.start();}
+    shooter.setFiringBlocked(
+        blocked&&waiter.hasElapsed(1));
   }
 
   @Override
