408d512f3b3aa0c4f3d90b4140a78f81a50a8d19
RedBirdRoboticsBot <114703348+Isma27322@users.noreply.github.com>
2024-02-16 17:00:13 -0600

    allowed different functions and modes for colors to be made

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ package frc.robot;
 import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -147,8 +148,8 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    driverController.b().onTrue(new StartEndCommand(() -> leds.setRed(), () -> {}));
-    driverController.a().onTrue(new StartEndCommand(() -> leds.setGreen(), () -> {}));
+    driverController.b().onTrue(new InstantCommand(() -> leds.mode1()));
+    driverController.a().onTrue(new InstantCommand(() -> leds.mode2()));
   }
 
   /**
--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -11,9 +11,13 @@ public class LEDs extends PeriodicRunnable {
   int colour = 0;
   int brightness = 25;
   int x = 5;
+  int x2 = 45;
   int y = 0;
   int a = 15;
   int time = 0;
+  int timespeed = 2;
+  boolean mode1 = false;
+  boolean mode2 = false;
 
   public LEDs() {
     super(); // Super call adds it to the registry, which calls the periodic method every tick
@@ -27,17 +31,22 @@ public class LEDs extends PeriodicRunnable {
 
   @Override
   public void periodic() {
-    
+    timespeed = 3; //bigger value makes stuff slower
+
     time++;
-    time = time % 2;
+    time = time % timespeed;
 
     brightness = (a - 15) * (a - 15);
 
+    if(time == 1)
+    {
     a++;
     a = a % 30;
+    }
 
     if (time == 1) {
       x = x + y % ledBuff.getLength();
+      x2 = x + -y % ledBuff.getLength();
     }
     if (x == 44) {
       y = -1;
@@ -47,28 +56,53 @@ public class LEDs extends PeriodicRunnable {
       y = 1;
     }
 
-    /*  for (var i = 0; i < 50; i++) { //mode 1
-      ledBuff.setHSV(i, (counter + i * 2) % 180, 255, brightness);
+    if(mode1 == true)
+    {  for (var i = 0; i < 50; i++) { //mode 1
+      ledBuff.setHSV(i, colour, 255, brightness);
 
     }
-    counter += 2; */
+    colour = (colour + 1) % 180;
 
-    // ledBuff.setHSV((x - (y * 5)), 7, 255, 0);
+  }
 
-    // ledBuff.setHSV(x, counter % 180, 255, brightness);
 
-    ledBuff.setHSV((50 + -x) + (-y * 5), colour, 255, 255);
+
+    if(mode2 == true)
+    {
+     ledBuff.setHSV((x - (y * 5)), 7, 255, 0);
+
+     ledBuff.setHSV(x, colour, 255, brightness);
+
+   // ledBuff.setHSV((50 + -x) + (-y * 5), colour, 255, 255);
     
-    ledBuff.setHSV(50 + -x, colour, 255, 0);
+  //  ledBuff.setHSV(50 + -x, colour, 255, 0);
+
+      ledBuff.setHSV((x2 - (-y * 5)), 7, 255, 0);
+
+     ledBuff.setHSV(x2, colour, 255, brightness);
+    
+
+    colour = (colour + 1) % 180;
+  }
+
     leds.setData(ledBuff);
   }
 
-  public void setRed() {
-    colour = 99;
+  public void mode1() {
+    mode1 = true;
+    mode2 = false;
   }
-  public void setGreen() {
-    System.out.println("Greeen");
-    colour = 64;
+  public void mode2() {
+    mode2 = true;
+    mode1 = false;
+  }
+
+  public void intakelight() {
+    colour = 1;
+  }
+
+  public void shooterlight() {
+    colour = 60;
   }
 
   /* private void resetLeds() {
