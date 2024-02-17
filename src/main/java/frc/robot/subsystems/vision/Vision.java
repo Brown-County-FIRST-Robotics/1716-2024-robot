210695d655580aa49451b4ba9ee9ff013fcb508e
Colin <cfinn68916@gmail.com>
2024-02-17 13:57:29 -0600

    Unstaged changes during deploy at 20240217_135729

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -65,27 +65,27 @@ public class Vision extends PeriodicRunnable {
                             inputs[i].pose.get()[3],
                             inputs[i].pose.get()[4],
                             inputs[i].pose.get()[5],
-                            inputs[i].pose.get()[6]))
-                    .rotateBy(new Rotation3d(0, 0, Math.PI));
+                            inputs[i].pose.get()[6])).unaryMinus().rotateBy(new Rotation3d(0,0,Math.PI));
             Pose3d tagpose =
                 layout.getTagPose(Integer.parseInt(inputs[i].ids.get()[0])).orElse(new Pose3d());
-            Rotation3d rot =
-                new Rotation3d(
-                    r1.getX(),
-                    r1.getY(),
-                    drivetrain
-                        .getPosition()
-                        .relativeTo(tagpose.toPose2d())
-                        .getRotation()
-                        .interpolate(r1.toRotation2d(), 0.02)
-                        .getRadians());
+//            Rotation3d rot =
+//                new Rotation3d(
+//                    r1.getX(),
+//                    r1.getY(),
+//                    drivetrain
+//                        .getPosition()
+//                        .relativeTo(tagpose.toPose2d())
+//                        .getRotation()
+//                        .interpolate(r1.toRotation2d(), 0.2)
+//                        .getRadians());
+            Rotation3d rot=r1;
 
             Transform3d as =
                 new Transform3d(
                     new Translation3d(
                         inputs[i].pose.get()[0], inputs[i].pose.get()[1], inputs[i].pose.get()[2]),
                     rot);
-            outPose = tagpose.plus(as);
+            outPose = tagpose.plus(as.inverse());
           } else if (inputs[i].ids.get().length > 1) {
             outPose =
                 new Pose3d(
