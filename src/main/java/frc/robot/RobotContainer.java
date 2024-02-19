db73f500c955e7c725add1155b2c39cfb54b0ae6
Colin <cfinn68916@gmail.com>
2024-02-19 17:04:45 -0600

    Unstaged changes during deploy at 20240219_170445

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -213,6 +213,8 @@ public class RobotContainer {
         .whileTrue(
             new SimpleSpeakerShoot(
                 driveSys, arm, teleopDrive::setCustomRotation, shooter, secondController.getHID()));
+    // Rapid eject
+    secondController.a().whileTrue(Commands.runEnd(()->{shooter.cmdFeeder(-8000);shooter.cmdVel(-1000,1000);},()->{shooter.cmdFeeder(0);},shooter));
   }
 
   /**
