db9c380483d8ce39fab73f434fc7889dbd278029
Colin <cfinn68916@gmail.com>
2024-02-22 18:44:18 -0600

    Unstaged changes during deploy at 20240222_184418

--- src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
+++ src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
@@ -29,11 +29,11 @@ public class ModuleIOSparkFX implements ModuleIO {
   double offset;
 
   String name;
-  LoggedTunableNumber thrustP = new LoggedTunableNumber("Thrust P", 0);
+  LoggedTunableNumber thrustP = new LoggedTunableNumber("Thrust P", 30.0/6380.0);
   LoggedTunableNumber thrustI = new LoggedTunableNumber("Thrust I", 0);
   LoggedTunableNumber thrustD = new LoggedTunableNumber("Thrust D", 0);
   LoggedTunableNumber thrustKV = new LoggedTunableNumber("Thrust KV", 60.0 / 6380.0);
-  LoggedTunableNumber steerP = new LoggedTunableNumber("Steer P", 0);
+  LoggedTunableNumber steerP = new LoggedTunableNumber("Steer P", 1.0/600.0);
   LoggedTunableNumber steerI = new LoggedTunableNumber("Steer I", 0);
   LoggedTunableNumber steerD = new LoggedTunableNumber("Steer D", 0);
   LoggedTunableNumber steerKV = new LoggedTunableNumber("Steer KV", 1.0 / 300.0);
@@ -62,9 +62,9 @@ public class ModuleIOSparkFX implements ModuleIO {
     } else if (thrustID == 21) {
       off = 0;
     } else if (thrustID == 22) {
-      off = 0;
+      off = -0.05;
     } else if (thrustID == 23) {
-      off = 0.5;
+      off = 0.45;
     }
     offsetTun.initDefault(off);
     config.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
