772bf2084606c5badc0bd24f6d5764098c4b670a
Colin <cfinn68916@gmail.com>
2024-02-23 18:35:46 -0600

    Unstaged changes during deploy at 20240223_183546

--- src/main/java/frc/robot/commands/Intake.java
+++ src/main/java/frc/robot/commands/Intake.java
@@ -16,7 +16,7 @@ public class Intake extends Command {
   Shooter shooter;
   Arm arm;
   XboxController controller;
-  static LoggedTunableNumber armPreset = new LoggedTunableNumber("Presets/Intake_Arm", -0.14);
+  static LoggedTunableNumber armPreset = new LoggedTunableNumber("Presets/Intake_Arm", -0.16);
   static LoggedTunableNumber topSpeed = new LoggedTunableNumber("Presets/Intake_Top", 1700);
   static LoggedTunableNumber bottomSpeed = new LoggedTunableNumber("Presets/Intake_Bottom", -2000);
   static LoggedTunableNumber armSourcePreset =
@@ -82,9 +82,9 @@ public class Intake extends Command {
   }
 
   private void setSpeedsAndPositions() {
-    if (!Overrides.disableArmAnglePresets.get()) {
+    if (!Overrides.disableArmAnglePresets.get() && !controller.getBButton()) {
       arm.setAngle(Rotation2d.fromRotations(preset.get()));
-    } else {
+    } else if(Overrides.disableArmAnglePresets.get()) {
       arm.commandIncrement(
           Rotation2d.fromRotations(
               controller.getLeftY() * Overrides.armAngleOverrideIncrementScale.get()));
