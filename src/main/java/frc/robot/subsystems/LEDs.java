3c986de9a45f7d61d5899ee0be67098f0622afaa
GldeA <glde.abbott@gmail.com>
2024-02-26 18:10:52 -0600

    Unstaged changes during deploy at 20240226_181052

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -19,15 +19,15 @@ public class LEDs extends PeriodicRunnable {
   int time = 0;
   int timespeed = 3;   //bigger value makes stuff slower
   int raindrop[] = new int[180];
-  boolean mode1 = false;
+  boolean mode1 = true;
   boolean mode2 = false;
   boolean mode3 = false;
   Random random = new Random();
 
   public LEDs() {
     super(); // Super call adds it to the registry, which calls the periodic method every tick
-    leds = new AddressableLED(5);
-    ledBuff = new AddressableLEDBuffer(100); // something around 280 length for LED
+    leds = new AddressableLED(0);
+    ledBuff = new AddressableLEDBuffer(54); // something around 280 length for LED
     leds.setLength(ledBuff.getLength());
     leds.setData(ledBuff);
     leds.start();
