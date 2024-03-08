75fc9d0fd926e0a33cce8178b292ea9cc39a8f18
Colin <cfinn68916@gmail.com>
2024-03-08 15:58:20 -0600

    Unstaged changes during deploy at 20240308_155820

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -80,8 +80,8 @@ public class RobotContainer {
                   driveSys,
                   new Transform3d[] {
                     new Transform3d(
-                        new Translation3d(0, 0 * 0.0254, 22 * 0.0254),
-                        new Rotation3d(0, -12.0 * Math.PI / 180, 0))
+                        new Translation3d(8*0.0254, 11 * 0.0254, 22 * 0.0254),
+                        new Rotation3d(0, -8.0 * Math.PI / 180, 0))
                   },
                   new VisionIO[] {new VisionIOSecondSight("SS_LAPTOP", "0")});
           break;
