ef17f928fae6b9933085cde1cce4390ce4e2e78c
Colin <cfinn68916@gmail.com>
2024-03-08 17:59:31 -0600

    Unstaged changes during deploy at 20240308_175931

--- src/main/java/frc/robot/subsystems/arm/ArmIOSparkFlex.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOSparkFlex.java
@@ -2,33 +2,40 @@ package frc.robot.subsystems.arm;
 
 import com.revrobotics.*;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.Constants;
 import frc.robot.utils.CustomAlerts;
 import frc.robot.utils.LoggedTunableNumber;
+import org.xml.sax.helpers.AttributesImpl;
 
 public class ArmIOSparkFlex implements ArmIO {
   CANSparkMax controller;
   SparkPIDController pid;
-  AbsoluteEncoder encoder;
+  AbsoluteEncoder absencoder;
   private static final double GEAR_RATIO = 25.0 * 72.0 / 15.0;
   private static final double FREE_RPM = 5676.0;
   LoggedTunableNumber ffTuner = new LoggedTunableNumber("Arm/ff_tuner", GEAR_RATIO / FREE_RPM);
-  LoggedTunableNumber pTuner = new LoggedTunableNumber("Arm/p_tuner", 0.5 * GEAR_RATIO / FREE_RPM);
+  LoggedTunableNumber pTuner = new LoggedTunableNumber("Arm/p_tuner", 0 * GEAR_RATIO / FREE_RPM);
   LoggedTunableNumber iTuner = new LoggedTunableNumber("Arm/i_tuner", 0.0);
   LoggedTunableNumber dTuner = new LoggedTunableNumber("Arm/d_tuner", 0.0);
   LoggedTunableNumber offset = new LoggedTunableNumber("Arm/offset", 0.496);
+  RelativeEncoder relencoder;
+  Timer nom=new Timer();
+  Rotation2d relEncoderZero=new Rotation2d();
 
   public ArmIOSparkFlex(int id) {
     controller = new CANSparkMax(id, CANSparkLowLevel.MotorType.kBrushless);
     pid = controller.getPIDController();
-    encoder = controller.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
+    absencoder = controller.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
+    relencoder=controller.getEncoder();
     controller.restoreFactoryDefaults();
-    encoder.setInverted(true);
+    relencoder.setPositionConversionFactor(1.0/GEAR_RATIO);
+    absencoder.setInverted(true);
     controller.setInverted(true);
 
     controller.setIdleMode(CANSparkBase.IdleMode.kBrake);
     controller.setSmartCurrentLimit(Constants.CurrentLimits.NEO);
-    pid.setFeedbackDevice(encoder);
+    pid.setFeedbackDevice(relencoder);
     pid.setOutputRange(-1, 1);
     pid.setSmartMotionMaxVelocity(FREE_RPM / GEAR_RATIO, 0);
     pid.setSmartMotionMinOutputVelocity(0, 0);
@@ -40,21 +47,53 @@ public class ArmIOSparkFlex implements ArmIO {
     dTuner.attach(pid::setD);
     controller.burnFlash();
     CustomAlerts.makeOverTempAlert(controller, 60, 50, "Arm motor");
+    rezero();
   }
 
   @Override
   public void updateInputs(ArmIOInputs inputs) {
-    inputs.angle = Rotation2d.fromRotations(encoder.getPosition() - offset.get());
-    inputs.omega = encoder.getVelocity();
+    inputs.angle = getProcessedRel();
+    inputs.omega = relencoder.getVelocity();
     inputs.appliedOutput = controller.getAppliedOutput();
     inputs.temperature = controller.getMotorTemperature();
     inputs.current=controller.getOutputCurrent();
+    if(relencoder.getVelocity()>0.1||absencoder.getVelocity()>0.1){
+      nom.restart();
+    }
+    if(nom.hasElapsed(0.5)){
+      rezero();
+    }
+  }
+  private Rotation2d getAbs(){
+    return Rotation2d.fromRotations(absencoder.getPosition() - offset.get());
+  }
+  private Rotation2d getUnprocessedRel(){
+    return Rotation2d.fromRotations(relencoder.getPosition());
+  }
+  private Rotation2d getProcessedRel(){
+    return relEncoderZero.plus(getUnprocessedRel());
+  }
+  private void rezero(){
+    relEncoderZero=getAbs().minus(getUnprocessedRel());
+    nom.restart();
+    System.out.println("REZWRE");
   }
 
   @Override
   public void setAngle(Rotation2d cmdAng, double arbFF) {
+    double adjustedRelCmd =relencoder.getPosition()
+            - (relencoder.getPosition() % 1.0)
+            + cmdAng.minus(relEncoderZero).getRotations();
+    if (Math.abs(adjustedRelCmd - relencoder.getPosition())
+        > Math.abs(1.0 + adjustedRelCmd - relencoder.getPosition())) {
+      adjustedRelCmd += 1.0;
+    }
+    if (Math.abs(adjustedRelCmd - relencoder.getPosition())
+        > Math.abs(-1.0 + adjustedRelCmd - relencoder.getPosition())) {
+      adjustedRelCmd -= 1.0;
+    }
     pid.setReference(
-        cmdAng.getRotations() + offset.get(),
+         adjustedRelCmd,
         CANSparkBase.ControlType.kSmartMotion,
         0,
         arbFF,
