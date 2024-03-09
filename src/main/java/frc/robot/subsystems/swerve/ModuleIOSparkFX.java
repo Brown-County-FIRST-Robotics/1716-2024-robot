e5fc3fe335e8031ee27e397a9105c019d6afd2f2
Colin <cfinn68916@gmail.com>
2024-03-09 11:27:20 -0600

    Unstaged changes during deploy at 20240309_112720

--- src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
+++ src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
@@ -30,7 +30,7 @@ public class ModuleIOSparkFX implements ModuleIO {
   double offset;
 
   String name;
-  LoggedTunableNumber thrustP = new LoggedTunableNumber("Thrust P", 30.0 / 6380.0);
+  LoggedTunableNumber thrustP = new LoggedTunableNumber("Thrust P", 1.6/(6380.0/60.0));
   LoggedTunableNumber thrustI = new LoggedTunableNumber("Thrust I", 0);
   LoggedTunableNumber thrustD = new LoggedTunableNumber("Thrust D", 0);
   LoggedTunableNumber thrustKV = new LoggedTunableNumber("Thrust KV", 60.0 / 6380.0);
@@ -53,11 +53,15 @@ public class ModuleIOSparkFX implements ModuleIO {
     this.name = name;
     thrust = new TalonFX(thrustID);
     TalonFXConfiguration config = new TalonFXConfiguration();
+    thrust.getConfigurator().refresh(config);
     config.Audio.BeepOnConfig = false;
     config.Audio.BeepOnBoot = false;
+    config.MotorOutput.PeakForwardDutyCycle=1;
+    config.MotorOutput.PeakReverseDutyCycle=-1;
     config.Audio.AllowMusicDurDisable = true;
     config.Slot0.kV = thrustKV.get();
-    thrust.getConfigurator().refresh(config.CustomParams);
+    config.Slot0.kP=thrustP.get();
+    config.MotorOutput.DutyCycleNeutralDeadband=0.01;
     offsetTun = new LoggedTunableNumber(name + "_offset");
     if (thrustID == 20) {
       off = 0.824;
@@ -76,10 +80,10 @@ public class ModuleIOSparkFX implements ModuleIO {
     posSignal = thrust.getRotorPosition();
     errSignal = thrust.getClosedLoopError();
     tempSignal = thrust.getDeviceTemp();
-    velSignal.setUpdateFrequency(50.0);
-    posSignal.setUpdateFrequency(50.0);
-    errSignal.setUpdateFrequency(50.0);
-    tempSignal.setUpdateFrequency(20.0);
+    velSignal.setUpdateFrequency(250.0);
+    posSignal.setUpdateFrequency(250.0);
+    errSignal.setUpdateFrequency(250.0);
+    tempSignal.setUpdateFrequency(250.0);
     thrust.optimizeBusUtilization();
     steer = new CANSparkMax(steerID, CANSparkLowLevel.MotorType.kBrushless);
     steer.restoreFactoryDefaults();
@@ -103,6 +107,8 @@ public class ModuleIOSparkFX implements ModuleIO {
     steerP.attach(pid::setP);
     steerI.attach(pid::setI);
     steerD.attach(pid::setD);
+    BaseStatusSignal.refreshAll(velSignal, posSignal, errSignal, tempSignal);
+
 
     steer.burnFlash();
     Logger.recordOutput("Firmware/" + name + "_Steer", steer.getFirmwareString());
