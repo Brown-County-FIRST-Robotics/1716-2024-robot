be0ae2a2154c255cfef6e8379d7ba9aa01e60045
Colin <cfinn68916@gmail.com>
2024-03-14 11:44:19 -0500

    Javadoc for AutoFactories
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- src/main/java/frc/robot/utils/AutoFactories.java
+++ src/main/java/frc/robot/utils/AutoFactories.java
@@ -18,7 +18,17 @@ import frc.robot.subsystems.shooter.Shooter;
 import java.util.List;
 import java.util.Optional;
 
+/** A bunch of static factory methods for creating auto routines */
 public class AutoFactories {
+  /**
+   * Makes a trajectory using the current position and velocity that goes to the given position with
+   * the ending tangent line slope
+   *
+   * @param drive The drivetrain
+   * @param target The target translation, and the direction of movement at the end of the
+   *     trajectory
+   * @return The trajectory to go to the given pose
+   */
   private static Trajectory makeTrajectory(Drivetrain drive, Pose2d target) {
     TrajectoryConfig trajectoryConfig = new TrajectoryConfig(5, 10);
     Rotation2d realAng;
@@ -41,17 +51,43 @@ public class AutoFactories {
         trajectoryConfig);
   }
 
+  /**
+   * Makes a trajectory using the current position and velocity that goes to the given position in a
+   * straight line
+   *
+   * @param drive The drivetrain
+   * @param target The target translation
+   * @return The trajectory to go to the given position
+   */
   private static Trajectory makeTrajectory(Drivetrain drive, Translation2d target) {
     return makeTrajectory(
         drive, new Pose2d(target, drive.getPosition().getTranslation().minus(target).getAngle()));
   }
 
+  /**
+   * Makes a command to shoot into the speaker
+   *
+   * @param drivetrain The drivetrain
+   * @param arm The arm
+   * @param shooter The shooter
+   * @return A command to rotate to and fire into the speaker
+   */
   public static Command speaker(Drivetrain drivetrain, Arm arm, Shooter shooter) {
     RotateTo rt = new RotateTo(drivetrain);
     SpeakerShoot speakerShoot = new SpeakerShoot(drivetrain, arm, rt::setCustomRotation, shooter);
     return speakerShoot.raceWith(rt.repeatedly());
   }
 
+  /**
+   * Makes a command to pick up a game piece
+   *
+   * @param drivetrain The drivetrain
+   * @param arm The arm
+   * @param shooter The shooter
+   * @param pos The game piece index
+   * @return A command that drives to the game piece and intakes it, but will also give up after a
+   *     second
+   */
   public static Command pickup(Drivetrain drivetrain, Arm arm, Shooter shooter, int pos) {
     Translation2d target = FieldConstants.getGamePiece(pos);
     HolonomicTrajectoryFollower trajectoryCommand =
@@ -72,7 +108,18 @@ public class AutoFactories {
                                         .getAngle()))))
                 .andThen(Commands.waitSeconds(1)));
   }
-
+  /**
+   * Makes a command to pick up a game piece. If it is not successful, it will attempt to pick up
+   * another game piece
+   *
+   * @param drivetrain The drivetrain
+   * @param arm The arm
+   * @param shooter The shooter
+   * @param pos The game piece index
+   * @param backupPos The game piece index to pick up if the first fails
+   * @return A command that drives to the game piece and attempts to intake it. If it fails, it will
+   *     attempt to pick up the second game piece
+   */
   public static Command pickupWithBackup(
       Drivetrain drivetrain, Arm arm, Shooter shooter, int pos, int backupPos) {
     return pickup(drivetrain, arm, shooter, pos)
