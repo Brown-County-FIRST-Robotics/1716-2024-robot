69b894c23d12b2c1ddedc3c11e4675f893d45048
Colin <cfinn68916@gmail.com>
2024-03-16 17:49:51 -0500

    Finish LoggedShuffleBoardBoolean
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- src/main/java/frc/robot/utils/shuffleboard/LoggedShuffleBoardBoolean.java
+++ src/main/java/frc/robot/utils/shuffleboard/LoggedShuffleBoardBoolean.java
@@ -1,25 +1,18 @@
 package frc.robot.utils.shuffleboard;
 
 import edu.wpi.first.networktables.BooleanSubscriber;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import java.util.ArrayList;
+import java.util.function.Consumer;
 import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedDashboardInput;
 
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.function.Consumer;
-
 public class LoggedShuffleBoardBoolean implements LoggedDashboardInput {
   @AutoLog
   public static class LoggedShuffleBoardBooleanInputs {
-    public boolean val =false;
+    public boolean val = false;
   }
 
   private final String key;
@@ -28,12 +21,12 @@ public class LoggedShuffleBoardBoolean implements LoggedDashboardInput {
   private LoggedShuffleBoardBooleanInputsAutoLogged inputs =
       new LoggedShuffleBoardBooleanInputsAutoLogged();
   private final ArrayList<Consumer<Boolean>> listeners = new ArrayList<>();
-  private String lastVal = "";
+  private boolean lastVal = false;
 
   public LoggedShuffleBoardBoolean(String tab, String key) {
     this.key = key;
-    widget=Shuffleboard.getTab(tab).add("",false);
-    subscriber= (BooleanSubscriber) widget.getEntry().getTopic().genericSubscribe("boolean");
+    widget = Shuffleboard.getTab(tab).add("", false);
+    subscriber = (BooleanSubscriber) widget.getEntry().getTopic().genericSubscribe("boolean");
 
     periodic();
     Logger.registerDashboardInput(this);
@@ -46,24 +39,17 @@ public class LoggedShuffleBoardBoolean implements LoggedDashboardInput {
   public void attach(Consumer<Boolean> listener) {
     listeners.add(listener);
     listener.accept(get());
-    Commands.runOnce()
   }
 
   @Override
   public void periodic() {
     if (!Logger.hasReplaySource()) {
-      inputs.val=subscriber.get(false);
+      inputs.val = subscriber.get(false);
     }
-
     Logger.processInputs("DashboardInputs/" + key, inputs);
-    if (inputs.selectedVal != null) {
-      if (!Objects.equals(inputs.selectedVal, lastVal) && !inputs.selectedVal.isEmpty()) {
-        for (var listener : listeners) {
-          listener.accept(get());
-        }
-        lastVal = inputs.selectedVal;
-      }
+    if (lastVal == inputs.val) {
+      listeners.forEach((listener) -> listener.accept(get()));
+      lastVal = inputs.val;
     }
   }
-
 }
