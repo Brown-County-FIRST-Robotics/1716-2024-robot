32e398f9a97369f926c7872cbf6ef4def5fca507
GldeA <glde.abbott@gmail.com>
2024-03-19 16:20:17 -0500

    Unstaged changes during deploy at 20240319_162016

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -51,10 +51,8 @@ public final class Constants {
 
   /** Constants relating to manual operation */
   public static final class Driver {
-    /** The maximum x velocity during manual operation in m/s */
-    public static final double MAX_X_SPEED = 5;
-    /** The maximum y velocity during manual operation in m/s*/
-    public static final double MAX_Y_SPEED = 5;
+    /** The maximum lateral velocity during manual operation in m/s */
+    public static final double MAX_SPEED = 5;
     /** The maximum angular velocity during manual operation in rad/s */
     public static final double MAX_THETA_SPEED = 10;
     /** The port the driver controller is on */
