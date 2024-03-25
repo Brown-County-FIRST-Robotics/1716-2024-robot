18d7060bfc4c803626beb6d70d3a6312ca916eb1
Colin <cfinn68916@gmail.com>
2024-03-25 12:17:58 -0500

    Arm framerate fix
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- src/main/java/frc/robot/subsystems/arm/ArmIOSparkFlex.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOSparkFlex.java
@@ -13,7 +13,7 @@ public class ArmIOSparkFlex implements ArmIO {
   private static final double GEAR_RATIO = 25.0 * 72.0 / 15.0;
   private static final double FREE_RPM = 5676.0;
   LoggedTunableNumber ffTuner = new LoggedTunableNumber("Arm/ff_tuner", GEAR_RATIO / FREE_RPM);
-  LoggedTunableNumber pTuner = new LoggedTunableNumber("Arm/p_tuner", 0 * GEAR_RATIO / FREE_RPM);
+  LoggedTunableNumber pTuner = new LoggedTunableNumber("Arm/p_tuner", 1.0 * GEAR_RATIO / FREE_RPM);
   LoggedTunableNumber iTuner = new LoggedTunableNumber("Arm/i_tuner", 0.0);
   LoggedTunableNumber dTuner = new LoggedTunableNumber("Arm/d_tuner", 0.0);
   LoggedTunableNumber offset = new LoggedTunableNumber("Arm/offset", 0.496);
@@ -38,6 +38,10 @@ public class ArmIOSparkFlex implements ArmIO {
     pTuner.attach(pid::setP);
     iTuner.attach(pid::setI);
     dTuner.attach(pid::setD);
+
+    controller.setPeriodicFramePeriod(CANSparkLowLevel.PeriodicFrame.kStatus5, 20);
+    controller.setPeriodicFramePeriod(CANSparkLowLevel.PeriodicFrame.kStatus6, 20);
+
     controller.burnFlash();
     CustomAlerts.makeOverTempAlert(controller, 60, 50, "Arm motor");
   }
