139306826155107fdfab60434ea6dd528eac02a7
Colin <cfinn68916@gmail.com>
2024-03-28 15:40:15 -0500

    Unstaged changes during deploy at 20240328_154015

--- src/main/java/frc/robot/utils/HolonomicTrajectoryFollower.java
+++ src/main/java/frc/robot/utils/HolonomicTrajectoryFollower.java
@@ -14,7 +14,7 @@ import org.littletonrobotics.junction.Logger;
 
 public class HolonomicTrajectoryFollower extends Command {
   private static final TrapezoidProfile.Constraints constraints =
-      new TrapezoidProfile.Constraints(6, 9);
+      new TrapezoidProfile.Constraints(4, 4);
   public static LoggedTunableNumber allowedErr = new LoggedTunableNumber("Rotation Allowed Err", 3);
   private static LoggedTunableNumber replanErr =
       new LoggedTunableNumber("Replanning threshold", 0.1);
@@ -36,7 +36,7 @@ public class HolonomicTrajectoryFollower extends Command {
                 new TrapezoidProfile.State(goal, 0))
             .velocity;
 
-    return tp.totalTime() > 0.02 ? fvel : 0;
+    return fvel;
   }
 
   Drivetrain drivetrain;
