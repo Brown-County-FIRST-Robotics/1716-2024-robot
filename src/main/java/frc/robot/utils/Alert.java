5cb61b178a5b6d2c8407e5dfc4cb3068cd5000ed
Colin <cfinn68916@gmail.com>
2024-03-29 16:16:00 -0500

    Unstaged changes during deploy at 20240329_161600

--- src/main/java/frc/robot/utils/Alert.java
+++ src/main/java/frc/robot/utils/Alert.java
@@ -10,11 +10,9 @@ package frc.robot.utils;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.LEDs;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -82,29 +80,30 @@ public class Alert {
           System.out.println(text);
           break;
       }
+      if (type == AlertType.WARNING) {
+        //      Commands.runOnce(() -> secondController.setRumble(RumbleType.kLeftRumble, 1.0))
+        //          .andThen(
+        //              Commands.waitSeconds(0.5)
+        //                  .andThen(
+        //                      Commands.runOnce(
+        //                          () -> secondController.setRumble(RumbleType.kLeftRumble, 0.0))))
+        //          .schedule();
+      }
+      if (type == AlertType.ERROR) {
+        LEDs.getInstance().mode1();
+        LEDs.getInstance().errorLight();
+
+        //      Commands.runOnce(() -> secondController.setRumble(RumbleType.kRightRumble, 1.0))
+        //          .andThen(
+        //              Commands.waitSeconds(0.5)
+        //                  .andThen(
+        //                      Commands.runOnce(
+        //                          () -> secondController.setRumble(RumbleType.kRightRumble,
+        // 0.0))))
+        //          .schedule();
+      }
     }
     this.active = active;
-    if (active && type == AlertType.WARNING) {
-      Commands.runOnce(() -> secondController.setRumble(RumbleType.kLeftRumble, 1.0))
-          .andThen(
-              Commands.waitSeconds(0.5)
-                  .andThen(
-                      Commands.runOnce(
-                          () -> secondController.setRumble(RumbleType.kLeftRumble, 0.0))))
-          .schedule();
-    }
-    if (active && type == AlertType.ERROR) {
-      LEDs.getInstance().mode1();
-      LEDs.getInstance().errorLight();
-
-      Commands.runOnce(() -> secondController.setRumble(RumbleType.kRightRumble, 1.0))
-          .andThen(
-              Commands.waitSeconds(0.5)
-                  .andThen(
-                      Commands.runOnce(
-                          () -> secondController.setRumble(RumbleType.kRightRumble, 0.0))))
-          .schedule();
-    }
   }
 
   /** Updates current alert text. */
