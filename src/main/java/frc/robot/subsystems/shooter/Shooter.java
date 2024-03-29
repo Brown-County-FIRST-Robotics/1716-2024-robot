a2fafe72ace8ba282533f61842db3fb294bbe622
Colin <cfinn68916@gmail.com>
2024-03-29 16:25:49 -0500

    Unstaged changes during deploy at 20240329_162549

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -13,7 +13,7 @@ public class Shooter extends SubsystemBase {
   ShooterIOInputsAutoLogged shooterInputs = new ShooterIOInputsAutoLogged();
   double cmdTopSpeed;
   double cmdBottomSpeed;
-  LoggedTunableNumber speedThreshold = new LoggedTunableNumber("Shooting speed threshold", 0.05);
+  LoggedTunableNumber speedThreshold = new LoggedTunableNumber("Shooting speed threshold", 0.02);
   LoggedTunableNumber firingTime = new LoggedTunableNumber("Firing Time", 0.5);
 
   boolean isShooting = false;
@@ -100,7 +100,7 @@ public class Shooter extends SubsystemBase {
   public void setSpeed(double exitVel) {
     double factor = 3500 / 9.88;
     cmdTopSpeed = -factor * exitVel;
-    cmdBottomSpeed = factor * exitVel;
+    cmdBottomSpeed = factor * exitVel+400;
   }
 
   public void shoot(double tvel, double bvel) {
