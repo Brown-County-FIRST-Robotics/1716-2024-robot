68d055ab11e2ee1b89a3942b4127010346049afa
Colin <cfinn68916@gmail.com>
2024-03-29 11:38:15 -0500

    Unstaged changes during deploy at 20240329_113815

--- src/main/java/frc/robot/utils/AutoFactories.java
+++ src/main/java/frc/robot/utils/AutoFactories.java
@@ -32,7 +32,7 @@ public class AutoFactories {
    * @return The trajectory to go to the given pose
    */
   private static Trajectory makeTrajectory(Drivetrain drive, Pose2d target) {
-    TrajectoryConfig trajectoryConfig = new TrajectoryConfig(5, 10);
+    TrajectoryConfig trajectoryConfig = new TrajectoryConfig(2, 10);
     Rotation2d realAng;
     if (ShootWhileMove.getFieldRelativeSpeeds(
                 drive.getVelocity(), drive.getPosition().getRotation())
@@ -69,6 +69,11 @@ public class AutoFactories {
   public static Command driveToPos(Drivetrain drivetrain, Translation2d target) {
     return new HolonomicTrajectoryFollower(drivetrain, () -> makeTrajectory(drivetrain, target));
   }
+
+  public static Command driveToPos(Drivetrain drivetrain, Pose2d target) {
+    return new HolonomicTrajectoryFollower(drivetrain, () -> makeTrajectory(drivetrain, target));
+  }
+
 
   /**
    * Makes a command to shoot into the speaker
