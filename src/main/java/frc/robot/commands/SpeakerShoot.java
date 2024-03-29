572e98dcea61a64199ee5537ec82f4f442c9a044
Colin <cfinn68916@gmail.com>
2024-03-29 10:38:21 -0500

    Unstaged changes during deploy at 20240329_103821

--- src/main/java/frc/robot/commands/SpeakerShoot.java
+++ src/main/java/frc/robot/commands/SpeakerShoot.java
@@ -37,7 +37,7 @@ public class SpeakerShoot extends Command {
                   new Transform3d(
                       new Translation3d(11 * 0.0254, 0, 10 * 0.0254),
                       new Rotation3d(0, -cmd.shooterAngle.getRadians(), 0)))
-              .transformBy(new Transform3d(new Translation3d(0.3, 0, 0.115), new Rotation3d()))
+              .transformBy(new Transform3d(new Translation3d(0.33, 0, 0.155), new Rotation3d()))
               .getTranslation();
 
   public SpeakerShoot(
@@ -63,32 +63,31 @@ public class SpeakerShoot extends Command {
   public void execute() {
     // Calculates position of the tip of the shooter
     Pose2d pos = drive.getPosition();
+    Rotation2d angleToSpeaker=FieldConstants.getSpeaker().toTranslation2d().minus(pos.getTranslation()).getAngle().minus(Rotation2d.fromDegrees(180));
     var cmd =
         ShootWhileMove.calcCommandWithKinematics(
             pos.getTranslation(),
-            FieldConstants.getSpeaker(),
+            FieldConstants.getSpeaker().plus(new Translation3d(0,angleToSpeaker.getDegrees()/300,0)),
             ShootWhileMove.getFieldRelativeSpeeds(
                 drive.getVelocity(), drive.getPosition().getRotation()),
             kinematics);
     shooter.setSpeed(9.88); // Max speed
     rotationCommander.accept(Optional.of(cmd.botAngle));
-    arm.setAngle(cmd.shooterAngle);
-    if (!Double.isNaN(cmd.shooterAngle.getRadians())) {
+    if (Double.isNaN(cmd.shooterAngle.getRadians())) {
+      cancel();
+    } else {
+      cmd.shooterAngle=cmd.shooterAngle.minus(Rotation2d.fromDegrees(6));
       arm.setAngle(cmd.shooterAngle);
     }
     // Prevent firing if angles are not close enough
     boolean blocked =
-        0.006 < Math.abs(cmd.botAngle.minus(drive.getPosition().getRotation()).getRotations())
+        0.01 < Math.abs(cmd.botAngle.minus(drive.getPosition().getRotation()).getRotations())
             || shooterAngleThreshold.get()
                 < Math.abs(cmd.shooterAngle.minus(arm.getAngle()).getRotations())
-            || drive.getVelocity().omegaRadiansPerSecond > 0.2;
+            || drive.getVelocity().omegaRadiansPerSecond > 0.5;
 
-    if (blocked) {
-      ft.restart();
-    }
-    boolean rb = !ft.hasElapsed(0.5);
-    shooter.setFiringBlocked(rb);
-    firing = firing || (!rb);
+    shooter.setFiringBlocked(blocked);
+    firing = firing || (!blocked);
   }
 
   @Override
