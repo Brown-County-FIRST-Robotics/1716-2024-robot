016d3eb5262ff10db254cf74f4e705498d882018
Colin <cfinn68916@gmail.com>
2024-03-29 14:54:43 -0500

    Unstaged changes during deploy at 20240329_145443

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -15,7 +15,7 @@ public class FieldConstants {
    */
   public static Translation3d getSpeaker() {
     // TEMP: fudge factors
-    return flip(new Translation3d(-.04 + 5 * .0254, 5.55, 80 * 0.0254));
+    return flip(new Translation3d(-.04 + 9 * .0254, 5.55, 80 * 0.0254));
   }
 
   public static Translation2d getGamePiece(int ind) {
