18b721fb65a0b63317c088f8c4993087181798d7
Colin <cfinn68916@gmail.com>
2024-03-29 12:37:32 -0500

    Unstaged changes during deploy at 20240329_123732

--- src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
+++ src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
@@ -32,7 +32,7 @@ public class ModuleIOSparkFX implements ModuleIO {
 
   String name;
   LoggedTunableNumber thrustP = new LoggedTunableNumber("Thrust P", 12.0 * 3.0 / (6380.0 / 60.0));
-  LoggedTunableNumber thrustI = new LoggedTunableNumber("Thrust I", 1);
+  LoggedTunableNumber thrustI = new LoggedTunableNumber("Thrust I", 0);
   LoggedTunableNumber thrustD = new LoggedTunableNumber("Thrust D", 0);
   LoggedTunableNumber thrustKV = new LoggedTunableNumber("Thrust KV", 12.0 * 60.0 / 6380.0);
   LoggedTunableNumber steerP = new LoggedTunableNumber("Steer P", 0);
@@ -62,6 +62,7 @@ public class ModuleIOSparkFX implements ModuleIO {
     config.Audio.AllowMusicDurDisable = true;
     config.Slot0.kV = thrustKV.get();
     config.Slot0.kP = thrustP.get();
+    config.Slot0.kI = thrustI.get();
     config.MotorOutput.DutyCycleNeutralDeadband = 0.01;
     offsetTun = new LoggedTunableNumber(name + "_offset");
     if (thrustID == 20) {
@@ -72,6 +73,8 @@ public class ModuleIOSparkFX implements ModuleIO {
       off = -0.03; // FL
     } else if (thrustID == 23) {
       off = 0.45; // FR
+    } else if (thrustID == 24) {
+      off = 0.069; // FR (backup)
     }
     offsetTun.initDefault(off);
     config.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
--- src/main/java/frc/robot/utils/AutoFactories.java
+++ src/main/java/frc/robot/utils/AutoFactories.java
@@ -32,12 +32,12 @@ public class AutoFactories {
    * @return The trajectory to go to the given pose
    */
   private static Trajectory makeTrajectory(Drivetrain drive, Pose2d target) {
-    TrajectoryConfig trajectoryConfig = new TrajectoryConfig(5, 10);
+    TrajectoryConfig trajectoryConfig = new TrajectoryConfig(2, 2);
     Rotation2d realAng;
     if (ShootWhileMove.getFieldRelativeSpeeds(
                 drive.getVelocity(), drive.getPosition().getRotation())
             .getNorm()
-        < 0.1) {
+        < 0.01) {
       realAng = target.getTranslation().minus(drive.getPosition().getTranslation()).getAngle();
     } else {
       var speed =
@@ -70,6 +70,10 @@ public class AutoFactories {
     return new HolonomicTrajectoryFollower(drivetrain, () -> makeTrajectory(drivetrain, target));
   }
 
+  public static Command driveToPos(Drivetrain drivetrain, Pose2d target) {
+    return new HolonomicTrajectoryFollower(drivetrain, () -> makeTrajectory(drivetrain, target));
+  }
+
   /**
    * Makes a command to shoot into the speaker
    *
@@ -97,9 +101,9 @@ public class AutoFactories {
   public static Command pickup(Drivetrain drivetrain, Arm arm, Shooter shooter, int pos) {
     Translation2d target = FieldConstants.getGamePiece(pos);
     HolonomicTrajectoryFollower trajectoryCommand =
-        new HolonomicTrajectoryFollower(drivetrain, () -> makeTrajectory(drivetrain, target));
+        new HolonomicTrajectoryFollower(drivetrain, () -> makeTrajectory(drivetrain, new Pose2d(target, Rotation2d.fromDegrees(0))));
 
-    return Intake.fromFloor(shooter, arm)
+    return new RotateTo(drivetrain,Rotation2d.fromDegrees(0)).andThen(Intake.fromFloor(shooter, arm)
         .raceWith(
             trajectoryCommand
                 .alongWith(
@@ -111,9 +115,9 @@ public class AutoFactories {
                                         .getPosition()
                                         .getTranslation()
                                         .minus(target)
-                                        .getAngle()))))
+                                        .getAngle().unaryMinus()))))
                 .andThen(Commands.waitSeconds(1))
-                .andThen(failedAlert::latch));
+                .andThen(failedAlert::latch)));
   }
   /**
    * Makes a command to pick up a game piece. If it is not successful, it will attempt to pick up
