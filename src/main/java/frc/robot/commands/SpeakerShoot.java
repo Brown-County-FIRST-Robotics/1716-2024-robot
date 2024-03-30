ba896f4ad656d5ae1253213e23ee26b04f8b0e01
Colin <cfinn68916@gmail.com>
2024-03-30 14:52:41 -0500

    Unstaged changes during deploy at 20240330_145241

--- src/main/java/frc/robot/commands/SpeakerShoot.java
+++ src/main/java/frc/robot/commands/SpeakerShoot.java
@@ -83,7 +83,7 @@ public class SpeakerShoot extends Command {
     if (Double.isNaN(cmd.shooterAngle.getRadians())) {
       cancel();
     } else {
-      cmd.shooterAngle = cmd.shooterAngle.minus(Rotation2d.fromDegrees(6));
+      cmd.shooterAngle = cmd.shooterAngle.minus(Rotation2d.fromDegrees(2));
       arm.setAngle(cmd.shooterAngle);
     }
     // Prevent firing if angles are not close enough
