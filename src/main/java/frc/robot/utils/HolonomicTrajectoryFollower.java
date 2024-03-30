5cfc23fa33e76e79677333ca14332c1dc3fdcbde
Colin <cfinn68916@gmail.com>
2024-03-29 20:56:14 -0500

    Fix replan error and turning profile constraints
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- src/main/java/frc/robot/utils/HolonomicTrajectoryFollower.java
+++ src/main/java/frc/robot/utils/HolonomicTrajectoryFollower.java
@@ -14,10 +14,10 @@ import org.littletonrobotics.junction.Logger;
 
 public class HolonomicTrajectoryFollower extends Command {
   private static final TrapezoidProfile.Constraints constraints =
-      new TrapezoidProfile.Constraints(6, 9);
+      new TrapezoidProfile.Constraints(10, 10);
   public static LoggedTunableNumber allowedErr = new LoggedTunableNumber("Rotation Allowed Err", 3);
-  private static LoggedTunableNumber replanErr =
-      new LoggedTunableNumber("Replanning threshold", 0.1);
+  private static final LoggedTunableNumber replanErr =
+      new LoggedTunableNumber("Replanning threshold", 0.4);
 
   public static double getExt(
       Rotation2d cmdRotation, Rotation2d currentRotation, double currentVelocity) {
