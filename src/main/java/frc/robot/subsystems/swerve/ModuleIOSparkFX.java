bc7ef14358a8e9ae47e3182fc2c275686ead4713
Colin <cfinn68916@gmail.com>
2024-03-31 10:29:57 -0500

    Unstaged changes during deploy at 20240331_102957

--- src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
+++ src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
@@ -35,11 +35,11 @@ public class ModuleIOSparkFX implements ModuleIO {
   LoggedTunableNumber thrustI = new LoggedTunableNumber("Thrust I", 0);
   LoggedTunableNumber thrustD = new LoggedTunableNumber("Thrust D", 0);
   LoggedTunableNumber thrustKV = new LoggedTunableNumber("Thrust KV", 12.0 * 60.0 / 6380.0);
-  LoggedTunableNumber steerP = new LoggedTunableNumber("Steer P", 1.0 / STEER_FREE_RPM);
+  LoggedTunableNumber steerP = new LoggedTunableNumber("Steer P", 0.0 / STEER_FREE_RPM);
   LoggedTunableNumber steerI = new LoggedTunableNumber("Steer I", 0);
   LoggedTunableNumber steerD = new LoggedTunableNumber("Steer D", 0);
   LoggedTunableNumber steerKV =
-      new LoggedTunableNumber("Steer KV", STEER_GEAR_RATIO / STEER_FREE_RPM);
+      new LoggedTunableNumber("Steer KV", 1.0 / STEER_FREE_RPM);
   LoggedTunableNumber offsetTun;
   double off;
 
