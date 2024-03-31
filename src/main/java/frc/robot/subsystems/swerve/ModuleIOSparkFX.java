7bb1a6d360efd46f76b8307b52590dcaacd3dae2
Colin <cfinn68916@gmail.com>
2024-03-31 10:51:50 -0500

    Unstaged changes during deploy at 20240331_105150

--- src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
+++ src/main/java/frc/robot/subsystems/swerve/ModuleIOSparkFX.java
@@ -39,7 +39,7 @@ public class ModuleIOSparkFX implements ModuleIO {
   LoggedTunableNumber steerI = new LoggedTunableNumber("Steer I", 0);
   LoggedTunableNumber steerD = new LoggedTunableNumber("Steer D", 0);
   LoggedTunableNumber steerKV =
-      new LoggedTunableNumber("Steer KV", STEER_GEAR_RATIO / STEER_FREE_RPM);
+      new LoggedTunableNumber("Steer KV", 1.5*STEER_GEAR_RATIO / STEER_FREE_RPM);
   LoggedTunableNumber offsetTun;
   double off;
 
@@ -93,6 +93,7 @@ public class ModuleIOSparkFX implements ModuleIO {
     thrust.optimizeBusUtilization();
     steer = new CANSparkMax(steerID, CANSparkLowLevel.MotorType.kBrushless);
     steer.restoreFactoryDefaults();
+    steer.setIdleMode(CANSparkBase.IdleMode.kBrake);
     pid = steer.getPIDController();
     analogEncoder = steer.getAnalog(SparkAnalogSensor.Mode.kAbsolute);
     relativeEncoder = steer.getEncoder();
@@ -105,7 +106,7 @@ public class ModuleIOSparkFX implements ModuleIO {
     pid.setOutputRange(-1, 1);
     pid.setSmartMotionMaxVelocity(STEER_FREE_RPM / STEER_GEAR_RATIO, 0);
     pid.setSmartMotionMinOutputVelocity(0, 0);
-    pid.setSmartMotionMaxAccel(10 * STEER_FREE_RPM / STEER_GEAR_RATIO, 0);
+    pid.setSmartMotionMaxAccel(10*STEER_FREE_RPM / STEER_GEAR_RATIO, 0);
     pid.setSmartMotionAllowedClosedLoopError(0.002, 0);
     steer.setSmartCurrentLimit(Constants.CurrentLimits.NEO);
 
