5c4eb868b895ab0eb2d7b9dec04c2db3f9e6fe92
GldeA <glde.abbott@gmail.com>
2024-04-06 13:53:46 -0500

    Add more autos
    Signed-off-by: GldeA <glde.abbott@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -241,6 +241,16 @@ public class RobotContainer {
             .andThen(AutoFactories.speaker(driveSys, arm, shooter)));
 
     autoChooser.addOption(
+        "3 note (1 then 0)",
+        AutoFactories.speaker(driveSys, arm, shooter)
+            .andThen(AutoFactories.pickup(driveSys, arm, shooter, 1))
+            .andThen(AutoFactories.driveToPos(driveSys, returningShotPos))
+            .andThen(AutoFactories.speaker(driveSys, arm, shooter))
+            .andThen(AutoFactories.pickup(driveSys, arm, shooter, 0))
+            .andThen(AutoFactories.driveToPos(driveSys, returningShotPos))
+            .andThen(AutoFactories.speaker(driveSys, arm, shooter)));
+
+    autoChooser.addOption(
         "4 note (2 1 0)",
         AutoFactories.speaker(driveSys, arm, shooter)
             .andThen(AutoFactories.pickup(driveSys, arm, shooter, 2))
@@ -254,6 +264,12 @@ public class RobotContainer {
             .andThen(AutoFactories.pickup(driveSys, arm, shooter, 0))
             .andThen(AutoFactories.driveToPos(driveSys, returningShotPos))
             .andThen(AutoFactories.speaker(driveSys, arm, shooter)));
+
+            
+    autoChooser.addOption(
+        "1 note",
+        AutoFactories.speaker(driveSys, arm, shooter)
+    );
   }
 
   /** Updates the pose estimator to use the correct initial pose */
