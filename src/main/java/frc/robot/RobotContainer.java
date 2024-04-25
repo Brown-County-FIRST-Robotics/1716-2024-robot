4413a4437c8a70d44bf1ba485219945cb7faf888
Colin <cfinn68916@gmail.com>
2024-04-25 16:38:00 -0500

    Unstaged changes during deploy at 20240425_163800

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -279,15 +279,13 @@ public class RobotContainer {
   }
 
   private void configureDemoBindings() {
-    secondController
-        .leftStick()
-        .whileFalse(
-            arm.run(
+    secondController.leftTrigger().whileTrue(            arm.run(
                 () ->
                     arm.commandIncrement(
                         Rotation2d.fromRotations(
-                            Overrides.armAngleOverrideIncrementScale.get()
-                                * secondController.getLeftY()))));
+                            -Overrides.armAngleOverrideIncrementScale.get()
+                                * TeleopDrive.deadScale(secondController.getLeftY())))).finallyDo(arm::commandNeutral));
+    secondController.rightTrigger().whileTrue(shooter.startEnd(()->shooter.shoot(-6700,6700),()->shooter.stop()));
   }
 
   private void configureCompBindings() {
@@ -427,7 +425,7 @@ public class RobotContainer {
         new ClimbAndLevel(
             climber, () -> -secondController.getRightY(), () -> driveSys.getGyro().getX()));
     secondController
-        .leftStick()
+        .rightStick().and(secondController.leftStick().negate())
         .whileTrue(
             new ClimbSplit(
                 climber, () -> -secondController.getLeftY(), () -> -secondController.getRightY()));
